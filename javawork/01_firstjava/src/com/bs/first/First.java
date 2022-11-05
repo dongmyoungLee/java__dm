694c67a92200d93d15f312ffd22c769206c1e5ef
dongmyounglee <pajang1515@daum.net>
2022-11-05 12:28:07 +0900

    22/11/05

--- javawork/01_firstjava/src/com/bs/first/First.java
+++ javawork/01_firstjava/src/com/bs/first/First.java
@@ -4,7 +4,8 @@ package com.bs.first;
 public class First {
 	
 //   java 를 실행하려면 무조건 선언되어야 하는 main method.. 한 파일에는 무조건 있어야 하고 main method가 종료될 때 끝남..
-	public static void main(String[] args) {	
-		System.out.println("Hello Wrold");
+	public static void main(String[] args) {
+		
 	}
 }
+
---
+++ javawork/01_firstjava/src/com/bs/variable/Variable.java
@@ -0,0 +1,24 @@
+package com.bs.variable;
+
+public class Variable {
+	// main method 만들기
+	public static void main(String[] args) {
+		// 변수선언하기
+		// RAM 영역에 사용할 공간을 확보하는 것
+		
+		// boolean 자료형은 1byte로 true, false 리터럴을 저장할 수 있음.
+		// 선언한 변수를 이용하려면 변수명을 사용한다.	
+		boolean flag;
+		flag = true;
+		System.out.println(flag);
+		flag = false;
+		System.out.println(flag);
+		
+		
+		
+		
+
+		
+		
+	}
+}
