2cd1bf0071a0ab0a48087c85f032212149a382a5
dongmyounglee <pajang1515@daum.net>
2022-11-05 12:30:40 +0900

    테스트커밋

--- javawork/01_firstjava/src/com/bs/variable/Variable.java
+++ javawork/01_firstjava/src/com/bs/variable/Variable.java
@@ -6,7 +6,7 @@ public class Variable {
 		// 변수선언하기
 		// RAM 영역에 사용할 공간을 확보하는 것
 		
-		// boolean 자료형은 1byte로 true, false 리터럴을 저장할 수 있음.
+		// boolean 자료형은 1byte로 true, false 리터럴을 저장할 수 있음!.
 		// 선언한 변수를 이용하려면 변수명을 사용한다.	
 		boolean flag;
 		flag = true;
