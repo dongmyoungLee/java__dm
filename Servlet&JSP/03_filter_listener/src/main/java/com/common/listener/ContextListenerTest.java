cfa7d8a39b8b7516d7f40b2d9e63c9f3e324e559
dongmyounglee <pajang1515@daum.net>
2023-03-04 16:12:51 +0900

    contextListener lifeCycle..

---
+++ Servlet&JSP/03_filter_listener/src/main/java/com/common/listener/ContextListenerTest.java
@@ -0,0 +1,60 @@
+package com.common.listener;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ContextListenerTest
+ *
+ */
+@WebListener
+public class ContextListenerTest implements ServletContextListener, ServletContextAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ContextListenerTest() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+     */
+    public void attributeAdded(ServletContextAttributeEvent scae)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+     */
+    public void attributeRemoved(ServletContextAttributeEvent scae)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("context 객체 소멸");
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+     */
+    public void attributeReplaced(ServletContextAttributeEvent event)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("context객체 생성");
+    }
+	
+}
