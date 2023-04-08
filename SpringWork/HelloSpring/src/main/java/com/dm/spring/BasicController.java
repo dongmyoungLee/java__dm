7bc6bd2bbd7868d9de31f39c2e046d6610b685a4
dongmyounglee <pajang1515@daum.net>
2023-04-08 16:44:01 +0900

    log4j layout mod..

--- SpringWork/HelloSpring/src/main/java/com/dm/spring/BasicController.java
+++ SpringWork/HelloSpring/src/main/java/com/dm/spring/BasicController.java
@@ -4,11 +4,18 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BasicController {
+	// log4j 로 출력하려면 log4j 가 제공하는 객체를 이용..
+	// Logger 클래스가 제공하는 메소드를 이용
+	// debug(), info(), warn(), error()
+	
+	private Logger logger = LoggerFactory.getLogger(BasicController.class);
 	
 	@RequestMapping("/")
 	public String mainView(HttpServletResponse response, HttpSession session) {
@@ -22,7 +29,15 @@ public class BasicController {
 		session.setAttribute("sessionId", "admin");
 		
 		
+		logger.debug("debug레벨 출력");
+		logger.info("infor 레벨 출력");
+		logger.warn("warn레벨 출력");
+		logger.error("error 레벨 출력");
 		
+		//logger 를 이용해서 메세지 출력시 String을 인수로 받는다.
+		// 객체를 출력할때는 매개변수가 두개인 메소드를 이용해야한다.
+		// 첫번째는 매개변수에 문자열로 {}, 두번째는 대입될 값을 선언
+		logger.info("{}", session);
 
 		return "index";
 	}
